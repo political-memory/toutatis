9e1d4c518fe29a423de00d8767f1bd166bff2ebf
Laurent Peuch <cortex@worlddomination.be>
2013-07-22 00:41:41 +0200

    [enh] VERY BIG move forward, start to curate the votes with very good results

---
+++ curating/management/commands/curate_votes.py
@@ -0,0 +1,182 @@
+# encoding: utf-8
+
+import sys
+from datetime import date, timedelta
+from itertools import izip_longest
+
+from django.core.management.base import BaseCommand
+from django.db import transaction, reset_queries
+
+from curating.models import CuratedProposalPart
+from parltrack_meps.models import Group, MEP, CountryMEP
+from parltrack_votes.models import ProposalPart
+
+group_convertion_table = {
+    "PPE": "EPP",
+    "S&D": "SD",
+    "Verts/ALE": "Greens/EFA",
+}
+
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    return izip_longest(fillvalue=fillvalue, *args)
+
+
+class Command(BaseCommand):
+    args = '<poll_id poll_id ...>'
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        a = 0
+        total = ProposalPart.objects.count()
+        #total = ProposalPart.objects.filter(datetime__gte=date(2013, 7, 1)).select_related('curatedproposalpart').count()
+        #for proposal_part in ProposalPart.objects.filter(datetime__gte=date(2013, 7, 1)).select_related('curatedproposalpart'):
+        for proposal_part in ProposalPart.objects.all().order_by('-datetime').select_related('curatedproposalpart'):
+            with transaction.commit_on_success():
+                #swaped_names = {}
+                a += 1
+                b = 0
+                fails = []
+
+                if proposal_part.curatedproposalpart is None:
+                    proposal_part.curatedproposalpart = CuratedProposalPart(proposal_part=proposal_part)
+                    proposal_part.curatedproposalpart.save()
+                    for chunck in grouper(set([x.mep for x in CountryMEP.objects.filter(begin__lte=proposal_part.datetime.date(), end__gte=proposal_part.datetime.date() - timedelta(days=1))]), 100):
+                        proposal_part.curatedproposalpart.meps.add(*filter(None, chunck))
+
+                    proposal_part.curatedproposalpart.save()
+
+                total_votes = proposal_part.vote_set.filter(mep__isnull=True).count()
+                for vote in proposal_part.vote_set.filter(mep__isnull=True):
+                    b += 1
+                    raw_mep = vote.raw_mep.replace(u"ß", "SS").replace("`", "'").replace("*", "")
+
+                    # the EP staff doesn't write their last last name
+                    if raw_mep == "Vitkauskaite":
+                        raw_mep = "VITKAUSKAITE BERNARD"
+                    if raw_mep == "Mathieu":
+                        raw_mep = "MATHIEU HOUILLON"
+                    if raw_mep == "Jordan Cizelj":
+                        raw_mep = "JORDAN"
+                    if raw_mep == "Kleva":
+                        raw_mep = u'KLEVA KEKU\u0160'
+                    if raw_mep == "Savisaar":
+                        raw_mep = "SAVISAAR-TOOMAST"
+                    if raw_mep == u'Mork\u016bnait\u0117':
+                        raw_mep = "MORKŪNAITĖ-MIKULĖNIENĖ"
+                    if raw_mep == u'Paksas* Rossi':
+                        raw_mep = "Paksas"
+
+                    # special unicode char
+                    if raw_mep in ("Rodriguez", "Perello Rodriguez"):
+                        raw_mep = u'PERELL\xd3 RODR\xcdGUEZ'
+                    if raw_mep == u'Sa\u010ffi':
+                        raw_mep = u'SA\xcfFI'
+                    if raw_mep in ('Grosset\xc3\xaate', u'Grosset\u0119te'):
+                        raw_mep = 'Grossetête'
+                    if raw_mep == u'Patr\u0103o Neves':
+                        raw_mep = "PATRÃO NEVES"
+                    if raw_mep == u'Gauz\u010ds':
+                        raw_mep = "GAUZÈS"
+                    if raw_mep == u'Estar\u0155s Ferragut':
+                        raw_mep = "ESTARÀS FERRAGUT"
+                    if raw_mep in (u'Gr\u010dze', u'Greze'):
+                        raw_mep = "GRÈZE"
+                    if raw_mep == u'J\u0159rgensen':
+                        raw_mep = "JØRGENSEN"
+                    if raw_mep == u'S\u0159ndergaard':
+                        raw_mep = "SØNDERGAARD"
+                    if raw_mep in (u'Scott\u0155', u"Scotta'"):
+                        raw_mep = "SCOTTÀ"
+                    if raw_mep == u'Estaras Ferragut':
+                        raw_mep = "ESTARÀS FERRAGUT"
+                    if raw_mep == u'Haefner':
+                        raw_mep = "HÄFNER"
+                    if raw_mep == u'Jimenez-Becerril Barrio':
+                        raw_mep = "JIMÉNEZ-BECERRIL BARRIO"
+                    if raw_mep == u'Y\xe1\u0144ez-Barnuevo Garc\xeda':
+                        raw_mep = "YÁÑEZ-BARNUEVO GARCÍA"
+
+                    # has changed of last name
+                    if raw_mep == "Manner":
+                        raw_mep = "PAKARINEN"
+                    if raw_mep == "Nedelcheva":
+                        raw_mep = "GABRIEL"
+                    if raw_mep == "Briard Auconie":
+                        raw_mep = "Auconie"
+                    if raw_mep == "Mihaylova":
+                        raw_mep = "NEYNSKY"
+
+                    sys.stdout.write("%s/%s %s/%s\r" % (a, total, b, total_votes))
+                    sys.stdout.flush()
+
+                    mep = proposal_part.curatedproposalpart.meps.filter(last_name__iexact=raw_mep)
+                    group = Group.objects.get(abbreviation=group_convertion_table.get(vote.raw_group, vote.raw_group))
+
+                    if not mep:
+                        #if swaped_names.get(group) is None:
+                            #swaped_names[group] = {"%s %s" % (x.last_name.lower(), x.first_name.lower()): x for x in proposal_part.curatedproposalpart.meps.all() if x.group() == group}
+                        #mep = [swaped_names[group].get(raw_mep.lower())] if swaped_names[group].get(raw_mep.lower()) else []
+                        for _mep in proposal_part.curatedproposalpart.meps.filter(swaped_name__isnull=True):
+                            _mep.swaped_name = "%s %s" % (_mep.last_name, _mep.first_name)
+                            _mep.save()
+                        mep = proposal_part.curatedproposalpart.meps.filter(swaped_name__iexact=raw_mep)
+
+                    if raw_mep in (u'Guerreiro', 'Rutowicz', u'Podka\u0144ski'):
+                        # FIXME I don't have those meps
+                        fails.append(raw_mep)
+                        continue
+
+                    if not mep:
+                        #print raw_mep
+                        #mep = raw_mep
+                        #meppp = proposal_part.curatedproposalpart.meps.filter(full_name__icontains=raw_mep)
+                        #mepdb = MEP.objects.filter(full_name__icontains=mep)
+                        #print "meppp", meppp
+                        #print "mepdb", mepdb
+                        #if mepdb:
+                            #print mepdb[0].full_name
+                            #print mepdb[0].last_name
+                            #print [(x, x.begin, x.end) for x in mepdb[0].countrymep_set.all()]
+                            #print proposal_part.datetime, proposal_part.id
+                        #meps = [x.last_name for x in proposal_part.curatedproposalpart.meps.all()]
+                        #import os
+                        #os.system("notify-send \"Can't link MEP '%s' to %s\"" % (mep, mepdb))
+                        #import time
+                        #time.sleep(3)
+                        #from IPython import embed; embed()
+                        #sys.exit(0)
+                        fails.append(raw_mep)
+                        continue
+
+                    if raw_mep in ('Le Pen', 'Winkler'):
+                        # FIXME I can't solve those one!
+                        fails.append(raw_mep)
+                        continue
+
+                    #print [x.abbreviation for x in Group.objects.all()]
+                    #print vote.raw_group
+                    if len(mep) != 1:
+                        mep = filter(lambda x: x.group() == group, mep)
+                        #assert mep
+
+                    #assert len(mep) == 1
+                    if len(mep) != 1:
+                        fails.append(raw_mep)
+                        continue
+                    vote.mep = mep[0]
+                    vote.save()
+
+                if proposal_part.vote_set.count():
+                    print "%s/%s %s%% %s %s          " % (a, total, int(proposal_part.vote_set.filter(mep__isnull=False).count() / float(proposal_part.vote_set.count()) * 100), proposal_part, proposal_part.datetime)
+                else:
+                    print "WAAAAAT, no votes on this proposal!", proposal_part
+
+                #if fails:
+                    #print "Fails:\n  * %s" % ("\n  * ".join(fails))
+
+                reset_queries()
+
+        #sys.stdout.write("\n")
