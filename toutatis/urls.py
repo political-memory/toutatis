a6bf655a5441b02252718b6c506e685b0e8f3bad
Olivier Le Thanh Duong <olivier@lethanh.be>
2013-11-02 12:59:16 +0100

    [fix] Restore the tastypie API

--- toutatis/urls.py
+++ toutatis/urls.py
@@ -1,6 +1,15 @@
 from django.conf.urls import patterns, include, url
 
+from tastypie.api import Api
+from api.api import ProposalRessource, ProposalPartRessource, VoteRessource, MEPRessource
+
+v1_api = Api(api_name='v1')
+v1_api.register(ProposalRessource())
+v1_api.register(ProposalPartRessource())
+v1_api.register(VoteRessource())
+v1_api.register(MEPRessource())
 
 urlpatterns = patterns('',
+    url(r'^api/', include(v1_api.urls)),
     url(r'^', include('exported_data.urls')),
 )
