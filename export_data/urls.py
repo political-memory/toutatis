501381b47c4d0caf585079674693f3c00a4f45a9
luxcem <af@laquadrature.net>
2015-05-12 17:44:57 +0200

    adds basic view for dossier export

--- export_data/urls.py
+++ export_data/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import patterns, url
 from export_data import views
 
 urlpatterns = patterns('',
-    url(r'^latest/checksum/$', views.get_lastest_checksum),
-    url(r'^latest/(?P<kind>\w+)/checksum/$', views.get_lastest_checksum),    
-    url(r'^latest/$', views.get_lastest_data),
-    url(r'^latest/(?P<kind>\w+)/$', views.get_lastest_data),
+    url(r'^(?P<dossier_reference>.+)/$', views.get_dossier_data),
+    # url(r'^latest/(?P<kind>\w+)/checksum/$', views.get_lastest_checksum),    
+    # url(r'^latest/$', views.get_lastest_data),
+    # url(r'^latest/(?P<kind>\w+)/$', views.get_lastest_data),
 )
--- export_data/views.py
+++ export_data/views.py
@@ -1,5 +1,9 @@
-from .models import ExportedRevision
 from django.http import HttpResponse, HttpResponseNotFound
+from .models import ExportedRevision
 
-def get_dossier_data(request, data):
-    pass
+def get_dossier_data(request, dossier_reference):
+     exported_revision = ExportedRevision.objects.filter(dossier_reference=dossier_reference)
+     if not exported_revision:
+         return HttpResponseNotFound('<h1>Page not found</h1>')
+
+     return HttpResponse(exported_revision.latest('last_check_datetime').data)
