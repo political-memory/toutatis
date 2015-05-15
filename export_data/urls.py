9e970f7596f40a2738ff15113dcf0ab4a1380897
luxcem <af@laquadrature.net>
2015-05-15 16:24:36 +0200

    adds url for export

--- export_data/urls.py
+++ export_data/urls.py
@@ -3,8 +3,5 @@ from django.conf.urls import patterns, url
 from export_data import views
 
 urlpatterns = patterns('',
-    url(r'^(?P<dossier_reference>.+)/$', views.get_dossier_data),
-    # url(r'^latest/(?P<kind>\w+)/checksum/$', views.get_lastest_checksum),    
-    # url(r'^latest/$', views.get_lastest_data),
-    # url(r'^latest/(?P<kind>\w+)/$', views.get_lastest_data),
+    url(r'^(?P<dossier_reference>.+)/$', views.get_dossier_data),       
 )
