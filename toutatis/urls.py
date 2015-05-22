21137ae7d072dbe411834b0048c69d37c876a43c
luxcem <af@laquadrature.net>
2015-05-22 13:17:13 +0200

    adds admin

--- toutatis/urls.py
+++ toutatis/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 from .views import home
+
 
 urlpatterns = [
     url(r'^$', home),
     url(r'^export/', include('export_data.urls')),
-    url(r'^api/', include('api.urls'))
+    url(r'^api/', include('api.urls')),
+    url(r'^admin/', include(admin.site.urls))
 ]
